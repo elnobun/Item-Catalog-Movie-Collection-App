279f82e08a0ff2abcc2c68079c64a987e7edee2b
Ellis Enobun <elnobun@gmail.com>
2016-07-10 09:20:37 +0100

    Update movie_app.py

--- movie_app.py
+++ movie_app.py
@@ -1,14 +1,12 @@
 from moviecollection import app
 import uuid
 
-"""
+```
 Item Catalog project main app.
-
 Run this file as 'python catalog_app.py' to initialize the server.
-
 Before running this app, ensure that the database is setup by running
 'database_setup.py'.
-"""
+```
 
 app.start_session()
 app.secret_key = uuid.uuid4().hex  # 'secret_key'
