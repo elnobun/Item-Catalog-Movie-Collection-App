a49ae99aee99fa63ec7c445fc1439234f235f1e2
Ellis Enobun <elnobun@gmail.com>
2016-09-07 09:13:26 +0000

    changed the port.

--- movie_app.py
+++ movie_app.py
@@ -8,8 +8,9 @@ import uuid
 app.start_session()
 app.secret_key = uuid.uuid4().hex  # 'secret_key'
 app.debug = False
+
 # If debug is enabled, the server will reload itself each time it notices a code change.
 # it is advisable to turn debug off in production.
 # The debugging console allows code to be executed directly on the server.
 
-app.run(host='0.0.0.0', port=9000)  # Tells the web-server on the vagrant machine to listen on all public IP-addresses.
+app.run(host='0.0.0.0', port=5000)  # Tells the web-server on the vagrant machine to listen on all public IP-addresses.
