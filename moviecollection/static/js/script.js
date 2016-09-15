719ee7921291935ad66c2e2cbade4f0056d89860
Ellis Enobun <elnobun@gmail.com>
2016-09-15 20:02:09 +0000

    New script for newMenu.html

--- moviecollection/static/js/script.js
+++ moviecollection/static/js/script.js
@@ -1,3 +1,17 @@
 /**
  * Created by Ellis on 15/09/2016.
  */
+
+//Script function for newMenu.html
+function source_select() {
+    if (document.getElementById('local_source').checked) {
+        document.getElementById('ifLocal').style.display = 'block';
+        document.getElementById('ifURL').style.display = 'none';
+    } else if (document.getElementById('url_source').checked) {
+        document.getElementById('ifURL').style.display = 'block';
+        document.getElementById('ifLocal').style.display = 'none';
+    } else if (document.getElementById('no_cover').checked) {
+        document.getElementById('ifLocal').style.display = 'none';
+        document.getElementById('ifURL').style.display = 'none';
+    }
+}
